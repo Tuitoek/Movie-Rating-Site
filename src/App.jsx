f05d1e9de52cc8207678320323c30ac62f3f46ea
Tuitoek <sjtoek@gmail.com>
2026-04-08 15:15:10 +0300

    Add to Like Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 
-const Card = ({ title, rating, isCool, actors }) => {
+const hasLiked = true;
+const Card = ({ title, rating, actors }) => {
+  const [hasLiked, setHasLiked] = useState(false);
   return (
     <div className="card">
       <h3>{title}</h3>
-      <h4>{rating}</h4>
-      <blockquote>{isCool ? 'This movie is cool!' : 'This movie is not really that cool.'}</blockquote>
+
+
+      {/* <blockquote>{isCool ? 'This movie is cool!' : 'This movie is not really that cool.'}</blockquote> */}
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "❤️" : "🤍"}
+      </button>
+
 
     </div>
   )
 }
 
 const App = () => {
+
+
   return (
     <div className='card-container'>
-      <Card title='Star Wars' rating={5} isCool={true} actors={['Harrison Ford', 'Mark Hamill', 'Carrie Fisher']} />
-      <Card title="Avatar" rating={4} isCool={false} actors={['Sam Worthington', 'Zoe Saldana']} />
-      <Card title="The Matrix" rating={4} isCool={false} actors={['Keanu Reeves', 'Laurence Fishburne']} />
+      <Card title='Star Wars' rating={5} actors={['Harrison Ford', 'Mark Hamill', 'Carrie Fisher']} />
+      <Card title="Avatar" rating={4} actors={['Sam Worthington', 'Zoe Saldana']} />
+      <Card title="The Matrix" rating={4} actors={['Keanu Reeves', 'Laurence Fishburne']} hasLiked={hasLiked} />
     </div>)
 }
 
