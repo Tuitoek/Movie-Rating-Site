f8dcedd834c593ebe9f4aaf5604f0d56159da58f
Tuitoek <sjtoek@gmail.com>
2026-04-09 08:20:41 +0300

    Add count state to card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const hasLiked = true;
 const Card = ({ title, rating, actors }) => {
+
+
   const [hasLiked, setHasLiked] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const handleCardClick = () => {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  });
+
   return (
-    <div className="card">
-      <h3>{title}</h3>
+    <div className="card" onClick={handleCardClick}>
+      <h3>{title} <br />{count}</h3>
 
 
       {/* <blockquote>{isCool ? 'This movie is cool!' : 'This movie is not really that cool.'}</blockquote> */}
@@ -23,7 +35,7 @@ const App = () => {
 
 
   return (
-    <div className='card-container'>
+    <div className='card-container' >
       <Card title='Star Wars' rating={5} actors={['Harrison Ford', 'Mark Hamill', 'Carrie Fisher']} />
       <Card title="Avatar" rating={4} actors={['Sam Worthington', 'Zoe Saldana']} />
       <Card title="The Matrix" rating={4} actors={['Keanu Reeves', 'Laurence Fishburne']} hasLiked={hasLiked} />
