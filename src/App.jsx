214616ea0adc8ef90c461a3bfc799d2039bd06be
Tuitoek <sjtoek@gmail.com>
2026-04-09 09:17:02 +0300

    Add functions for managing states

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,39 @@ import './App.css'
 const hasLiked = true;
 const Card = ({ title, rating, actors }) => {
 
-
+  // Initialize state for heart like
   const [hasLiked, setHasLiked] = useState(false);
+
+  // Initialize state for click count
   const [count, setCount] = useState(0);
 
+  // Handle the click event on the card
   const handleCardClick = () => {
+
     setCount((prevCount) => prevCount + 1);
   }
 
+  // Handle the click event on the heart button
+  const handleHeartClick = (e) => {
+    // This prevents 'handleCardClick' from ever seeing this event
+    e.stopPropagation();
+
+    setHasLiked(!hasLiked);
+  };
+
+  // Hook that runs after every render, logging the current like status
   useEffect(() => {
     console.log(`${title} has been liked: ${hasLiked}`);
   });
 
   return (
     <div className="card" onClick={handleCardClick}>
-      <h3>{title} <br />{count}</h3>
+      <h3>{title}</h3>
 
-
+      <p className='click'>Clicks: {count}</p>
       {/* <blockquote>{isCool ? 'This movie is cool!' : 'This movie is not really that cool.'}</blockquote> */}
-      <button onClick={() => setHasLiked(!hasLiked)}>
+
+      <button onClick={handleHeartClick}>
         {hasLiked ? "❤️" : "🤍"}
       </button>
 
